4a1f7e6d8cc1f6594b880964e877bf59600c2200
didar07 <didarul111015@gmail.com>
2022-09-28 21:09:45 +0600

    first commit

---
+++ src/Components/Exercise/Exercise.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Exercise = () => {
+    return (
+        <div>
+            <div className='main-container'>
+                <h1>main</h1>
+            </div>
+            <div className='cart-container'>
+                <h2>cart</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Exercise;
